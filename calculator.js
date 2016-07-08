a4fcce694e1b8f6313c590bbe045a5b154f2e7c9
Kamon <Harrellkamon@gmail.com>
2016-07-08 17:36:03 +1200

    kamons version of styling

--- calculator.js
+++ calculator.js
@@ -1,14 +1,4 @@
-/*User Story: I can add, subtract, multiply and divide two numbers.
-
-User Story: I can clear the input field with a clear button.
-
-User Story: I can keep chaining mathematical operations together until I hit the equal button, and the calculator will tell me the correct output.
-*/
-
-// Use to store memory (Should this be an array?)
 var memory = []
-
- // define listeners for each button (click only OR click and keypress?)
 
 addListeners(document.getElementsByClassName('board')[0].children)
 function addListeners (element) {
@@ -20,14 +10,13 @@ function addListeners (element) {
     element[i].addEventListener('click', allClear)
   }
 }
-// The calculator's update window
+//  The calculator's update window
 function readout (event) {
   if (event.target.classList.contains('number') || event.target.classList.contains('operator')) {
     document.getElementsByClassName('display')[0].innerHTML += event.target.innerHTML
   }
 }
-
-// Chain numbers on the display window
+//  Chain numbers on the display window
 function updateMemory (event) {
   if (event.target.classList.contains('number')) {
     memory.push(event.target.innerHTML)
@@ -35,21 +24,19 @@ function updateMemory (event) {
     memory.push(event.target.innerHTML)
   }
 }
-
-// Use operator given operator on numbers
+//  Run the current equation on screen
 function equals (event) {
   if (event.target.classList.contains('equals')) {
-      var result = eval(memory.join(''))
-      document.getElementsByClassName('display')[0].innerHTML = result
-      memory = [result]
+    var result = eval(memory.join(''))
+    document.getElementsByClassName('display')[0].innerHTML = result
+    memory = [result]
   }
 }
-
 // Clear memory
 function allClear (event) {
- if (event.target.classList.contains('all-clear')) {
-  document.getElementsByClassName('display')[0].innerHTML = 
-  memory = []
- } 
+  if (event.target.classList.contains('all-clear')) {
+    document.getElementsByClassName('display')[0].innerHTML =
+    memory = []
+  }
 }
 
