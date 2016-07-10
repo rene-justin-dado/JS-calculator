8bfd718bf1ac3076c01cd0c252bae88fa8cb9dd0
rene-justin-dado <justindado@yahoo.com>
2016-07-10 21:07:15 +1200

    tried to add percent, will retry later on

--- calculator.js
+++ calculator.js
@@ -11,14 +11,22 @@ function addListeners (element) {
 }
 //  The calculator's update window
 function readout (event) {
-  if (event.target.classList.contains('number') || event.target.classList.contains('operator')) {
-    document.getElementsByClassName('display')[0].innerHTML += event.target.innerHTML
-    memory.push(event.target.innerHTML)
+// Check the percent difference
+  if (this.classList.contains('percent')) {
+    //Make the last number in memory /100 and replace existing last number
+    var splitAtOperator = memory.join('').search(/\+|\-|\/|\*/g) + 1
+    var percentOperation = '(' + memory.join('').substring(splitAtOperator, memory.length) + '/100)' + '*' + memory.join('').substring(0, splitAtOperator - 1)
+    memory.splice(splitAtOperator, memory.length, percentOperation)
+  }
+
+  if (this.classList.contains('number') || this.classList.contains('operator')) {
+    document.getElementsByClassName('display')[0].innerHTML += this.innerHTML
+    memory.push(this.innerHTML)
   }
 }
 //  Run the current equation on screen
 function equals (event) {
-  if (event.target.classList.contains('equals')) {
+  if (this.classList.contains('equals')) {
     var result = eval(memory.join(''))
     document.getElementsByClassName('display')[0].innerHTML = result
     memory = [result]
@@ -26,7 +34,7 @@ function equals (event) {
 }
 // Clear memory
 function allClear (event) {
-  if (event.target.classList.contains('all-clear')) {
+  if (this.classList.contains('all-clear')) {
     document.getElementsByClassName('display')[0].innerHTML = ''
     memory = []
   }
